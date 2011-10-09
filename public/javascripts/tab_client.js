27278d0b2bf96ce62df4c2506be42e912f5d7858
Nick Carneiro <burthawk101@gmail.com>
2011-10-09 18:41:22 -0500

    added test data, division entry screen, division dropdown on teams screen

--- public/javascripts/tab_client.js
+++ public/javascripts/tab_client.js
@@ -85,9 +85,23 @@ tab.model.Round = Backbone.Model.extend({
 
 
 tab.model.Division = Backbone.Model.extend({
+	default: {
+		id				: null ,
+		division_name	: "VCX" ,  //eg: VCX, NLD
+		comp_per_team	: 2 , //number of competitors per team. 2 in CX, 1 in LD
+		break_to		: "quarterfinals" , //quarterfinals, octofinals, etc.
+		prelim_judges	: 1 , //number of judges in prelims
+		record_ranks	: true ,
+		max_speaks		: 30 , //maximum speaker points possible
+		flighted_rounds : false ,
+		prelims			: 4 , //
+		prelim_matching : []
+	} ,
 	initialize: function(){
-	            console.log("initialized division");
-	        }
+		this.set({
+			id: new ObjectId().toString()
+		});
+	}
 });
 
 
@@ -168,26 +182,66 @@ tab.view.Team = Backbone.View.extend({
 	unrender: function(){
 		$(this.el).remove();
 	}
-})
-tab.view.teamTable = Backbone.View.extend({
+});
+
+tab.view.DivisionOption = Backbone.View.extend({
+	tagName: "option",
+	initialize: function(){
+		_.bindAll(this, "render", "unrender", "remove");
+	    this.model.bind('remove', this.unrender);
+		this.model.bind('change', this.render);
+
+	} ,
+	remove: function(division){
+		this.model.destroy();
+	} ,
+	render: function(){
+		//associate data element "id" with ObjectId in case we want to use this later
+		$(this.el).data("id", this.model.get("id"));
+		//set the value attr to the ObjectId
+		//This will be read by jQuery to figure out which division was selected
+		$(this.el).attr("value", this.model.get("id"));
+		$(this.el).html(this.model.get("division_name"));
+		return this; //required for chainable call, .render().el ( in appendTeam)
+	} ,
+	unrender: function(){
+		$(this.el).remove();
+	}
+});
+tab.view.TeamTable = Backbone.View.extend({
 	el: $("#teams") , // attaches `this.el` to an existing element.
 	events: {
 		"click #add_team_button": "addTeam",
 		"keyup #teams_search": "search"
 	} ,
 	initialize: function(){
-		_.bindAll(this, "render", "addTeam", "appendTeam", "renderSearch", "search");
+		_.bindAll(this, "render", "addTeam", "appendTeam", 
+			"renderSearch", "search", "addDivSelect");
 		
-		tab.collection.teams = new tab.collection.Teams();
+		
 		tab.collection.teams.bind("add", this.appendTeam);
+
+		//keep division dropdown box up to date
+		tab.collection.divisions.bind("add", this.addDivSelect);
 		this.render();
 		
 	} ,
-	
+	//add new division to dropdown box
+	addDivSelect: function(division){
+		var divOptionView = new tab.view.DivisionOption({
+			model: division
+		});
+		$("#newteam_division", this.el).append(divOptionView.render().el);
+	} ,
 	render: function(){
-		_(tab.collection.teams).each(function(team){ // in case collection is not empty
+		_(tab.collection.teams).each(function(team){ // for pre-existing teams
         	appendTeam(team);
     	}, this);
+
+    	_(tab.collection.divisions).each(function(division){ // pre-existing divisions
+        	addDivSelect(division);
+    	}, this);
+
 	} ,
 
 	renderSearch: function(results){
@@ -256,7 +310,7 @@ tab.view.School = Backbone.View.extend({
 });
 
 
-tab.view.schoolTable = Backbone.View.extend({
+tab.view.SchoolTable = Backbone.View.extend({
 	el: $("#schools") , // attaches `this.el` to an existing element.
 	events: {
 		"click #add_school_button": "addSchool" ,
@@ -265,7 +319,6 @@ tab.view.schoolTable = Backbone.View.extend({
 	initialize: function(){
 		_.bindAll(this, "render", "addSchool", "appendSchool");
 		
-		tab.collection.schools = new tab.collection.Schools();
 		tab.collection.schools.bind("add", this.appendSchool);
 		this.render();
 		
@@ -312,15 +365,162 @@ tab.view.schoolTable = Backbone.View.extend({
 	
 });
 
+tab.view.Division = Backbone.View.extend({
+	tagName: "tr" ,
+	events: { 
+      'click td.remove': 'remove'
+    },  
+
+	initialize: function(){
+		_.bindAll(this, "render", "unrender", "remove");
+	    this.model.bind('remove', this.unrender);
+		this.model.bind('change', this.render);
+
+	} ,
+	remove: function(division){
+		this.model.destroy();
+	} ,
+	render: function(){
+		$(this.el).html('<td>' + this.model.get("division_name") + '</td> <td>' + this.model.get("id") + '</td><td class="remove">Remove</td>');
+		return this; //required for chainable call, .render().el ( in appendTeam)
+	} ,
+	unrender: function(){
+		$(this.el).remove();
+	}
+});
+
+
+tab.view.DivisionTable = Backbone.View.extend({
+	el: $("#divisions") , // attaches `this.el` to an existing element.
+	events: {
+		"click #add_division_button": "addDivision" 
+	} ,
+	initialize: function(){
+		_.bindAll(this, "render", "addDivision", "appendDivision");
+		
+		tab.collection.divisions.bind("add", this.appendDivision);
+		this.render();
+		
+	} ,
+	
+	render: function(){
+		_(tab.collection.divisions.models).each(function(division){ // in case collection is not empty
+        	appendDivision(division);
+    	}, this);
+	} ,
+
+	addDivision: function(){
+		//TODO: validate school name
+	
+
+
+		var division = new tab.model.Division();
+		//TODO: verify all this input
+		var division_name = $("#newdiv_division_name").val();
+		var comp_per_team = parseInt($("#newdiv_comp_per_team").val(), 10);
+		//TODO: verify that this boolean works
+		var flighted_rounds = new Boolean($("#newdiv_flighted_rounds").val());
+		var break_to = $("#newdiv_break_to").val();
+		var max_speaks = parseInt($("#newdiv_max_speaks").val());
+		var prelims = parseInt($("#newdiv_prelims").val());
+		division.set({
+			id				: (new ObjectId).toString(),
+			division_name	: division_name,
+			comp_per_team	: comp_per_team,
+			flighted_rounds	: flighted_rounds,
+			break_to		: break_to,
+			max_speaks		: max_speaks,
+			prelims			: prelims
+
+		});
+
+		tab.collection.divisions.add(division);
+		$("#newdiv_division_name").val("");
+		$("#newdiv_comp_per_team").val("");
+		$("#newdiv_division_name").val("");
+		$("#newdiv_division_name").val("");
+	} ,
+
+	appendDivision: function(division){
+		var divisionView = new tab.view.Division({
+			model: division
+		});
+		$("#divisions_table", this.el).append(divisionView.render().el);
+	}
+	
+});
 
 /*
 =========================================
-Initialize Backbone Views
+Initialize Backbone Collections, then Views
 =========================================
 */	
+tab.collection.divisions = new tab.collection.Divisions();
+tab.collection.teams = new tab.collection.Teams();
+tab.collection.schools = new tab.collection.Schools();
 
-tab.view.teamTable = new tab.view.teamTable(); 
-tab.view.schoolTable = new tab.view.schoolTable(); 
+
+tab.view.teamTable = new tab.view.TeamTable(); 
+tab.view.schoolTable = new tab.view.SchoolTable(); 
+tab.view.divisionTable = new tab.view.DivisionTable(); 
+/*
+=========================================
+Initialize Backbone Collections with test data
+=========================================
+*/	
+//creating a lot of one-time variables, so putting them in
+//anonymous self executing function
+(function(){
+	var div1 = new tab.model.Division();
+	div1.set({
+		division_name			: "VCX" ,  //eg: VCX, NLD
+		comp_per_team	: 2 , //number of competitors per team. 2 in CX, 1 in LD
+		break_to		: "quarterfinals" , //quarterfinals, octofinals, etc.
+		prelim_judges	: 1 , //number of judges in prelims
+		record_ranks	: true ,
+		max_speaks		: 30 , //maximum speaker points possible
+		flighted_rounds : false ,
+		prelims			: 4 , //
+		prelim_matching : []
+	});
+
+	var div2 = new tab.model.Division();
+	div2.set({
+		id				: (new ObjectId).toString() ,
+		division_name			: "NCX" ,  //eg: VCX, NLD
+		comp_per_team	: 2 , //number of competitors per team. 2 in CX, 1 in LD
+		break_to		: "quarterfinals" , //quarterfinals, octofinals, etc.
+		prelim_judges	: 1 , //number of judges in prelims
+		record_ranks	: true ,
+		max_speaks		: 30 , //maximum speaker points possible
+		flighted_rounds : false ,
+		prelims			: 4 , //
+		prelim_matching : []
+	});
+	tab.collection.divisions.add(div1);
+	tab.collection.divisions.add(div2);
+
+	var school1 = new tab.model.School();
+	school1.set({
+		school_name: "Round Rock"
+	});
+	var school2 = new tab.model.School();
+	school2.set({
+		school_name: "West Wood"
+	});
+	var school3 = new tab.model.School();
+	school3.set({
+		school_name: "Hendrickson"
+	});
+	var school4 = new tab.model.School();
+	school4.set({
+		school_name: "McNeil"
+	});
+
+	tab.collection.schools.add([school1, school2, school3, school4]);
+})();
+
+
 
 //initialize dropdowns
 $('#new_team_school').autocomplete({
@@ -378,7 +578,17 @@ $("#menu_schools").click(function(){
 	$("#sub_menu_schools").show();
 });	
 
+$("#menu_divisions").click(function(){
+	
+	$(".container").hide();
 
+	$(".menu_item").removeClass("menu_item_selected");
+	$("#menu_divisions").addClass("menu_item_selected");
+
+	$("#divisions_container").show();
+	$(".sub_menu").hide();
+	$("#sub_menu_divisions").show();
+});	
 
 /*
 =========================================
