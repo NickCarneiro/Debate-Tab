9a60b7b7e6ad675598c2cb8b4cd5de8ac06d97e1
Nick Carneiro <burthawk101@gmail.com>
2011-11-02 18:14:44 -0500

    moved PDF stuff to debug container. styled debug stuff into separate categories. tweaked console

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var Models = require('./models'); //include models
 var mongoose = require('mongoose'); //like include
 
 //Change port variable to your port when testing:
-var port = 3007;
+var port = 3003;
 /*
 'ali.debatetab.com': '127.0.0.1:3001',
 'rohan.debatetab.com': '127.0.0.1:3002',
